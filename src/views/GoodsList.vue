86c10c8e826e27e326259bc7098f1cc705177437
wmq2015 <121452295@qq.com>
2017-11-08 17:42:52 +0800

    做到模态框组件这里

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -11,9 +11,9 @@
                     <span class="sortby">Sort by:</span>
                     <a href="javascript:void(0)" class="default cur">Default</a>
                     <a @click="sortGoods" href="javascript:void(0)" class="price">
-                        Price 
-                        <svg class="icon icon-arrow-short">
-                            <use xlink:href="#icon-arrow-short"></use>
+                        Price
+                        <svg class="icon icon-arrow-short" :class="{'sort-up': !sortFlag }">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-arrow-short"></use>
                         </svg>
                     </a>
                     <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterShop">Filter by</a>
@@ -27,7 +27,9 @@
                                 <a href="javascript:void(0)" :class="{'cur': priceChecked=='all'}" @click="setPriceFilter('all')">All</a>
                             </dd>
                             <dd v-for="(price,index) in priceFilter" :key="index">
-                                <a href="javascript:void(0)" :class="{'cur': priceChecked==index}" @click="setPriceFilter(index)">{{price.startPrice}} - {{price.endPrice}}</a>
+                                <a href="javascript:void(0)" :class="{'cur': priceChecked==index}" @click="setPriceFilter(index)">
+                                    {{price.startPrice}} - {{price.endPrice}}
+                                </a>
                             </dd>
                         </dl>
                     </div>
@@ -46,7 +48,7 @@
                                         <div class="name" v-html="item.productName"></div>
                                         <div class="price" v-html="item.salePrice"></div>
                                         <div class="btn-area">
-                                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                                            <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                                         </div>
                                     </div>
                                 </li>
@@ -60,6 +62,20 @@
             </div>
         </div>
         <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+        <modal v-bind:mdShow=" mdShowCart || mdShow " v-on:close="closeModal">
+            <p slot="message">
+                <svg class="icon icon-status-ok" :class="{'sort-up': !sortFlag }" v-if="mdShowCart">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+                </svg>
+                <span v-if="mdShowCart">加入购物车成功!</span>
+                <span v-if="mdShow">请先登录!</span>
+            </p>
+            <div slot="btnGroup">
+                <a href="javascript:void(0)" class="btn btn--m" @click=" mdShowCart = false " v-if="mdShowCart">继续购物</a>
+                <router-link class="btn btn--m" href="javascript:void(0)" to="/cart" v-if="mdShowCart">查看购物车</router-link>
+                <a href="javascript:void(0)" class="btn btn--m" @click=" mdShow = false " v-if="mdShow">关 闭</a>
+            </div>
+        </modal>
         <nav-footer></nav-footer>
     </div>
 </template>
@@ -68,6 +84,17 @@
         height: 100px;
         line-height: 100px;
         text-align: center
+    }
+    .icon-arrow-short{
+        transition: all .3s ease-out;
+    }
+    .sort-up{
+        transform: rotate(180deg);
+        transition: all .3s ease-out;
+    }
+    .btn:hover{
+        background-color: #ffe5e6;
+        transition: all .3s ease-out
     }
 </style>
 
@@ -78,6 +105,7 @@
     import NavHeader from './../components/NavHeader'
     import NavBread from '@/components/NavBread'
     import NavFooter from '@/components/NavFooter'
+    import Modal from '@/components/Modal'
     import axios from 'axios'
     export default {
         data() {
@@ -89,9 +117,11 @@
                 pageSize: 8,
                 busy: true,
                 loading: false,
+                mdShow: false,
                 priceLevel: 'all',
                 filterBy: false,
                 overLayFlag: false ,
+                mdShowCart: false,
                 priceFilter:[
                     {
                         startPrice: '0.00',
@@ -118,7 +148,8 @@
         components: {
             NavHeader,
             NavBread,
-            NavFooter
+            NavFooter,
+            Modal
         },
         methods:{
             showFilterShop(){
@@ -137,7 +168,7 @@
                     priceLevel: this.priceLevel
                 };
                 this.loading = true;
-                axios.get('/goods',{
+                axios.get('/goods/list',{
                     params: param
                 }).then((response)=>{
                     let res = response.data;
@@ -177,6 +208,21 @@
                     this.page ++
                     this.getGoodsList(true);
                 }, 500);
+            },
+            addCart(productId){
+                axios.post('/goods/addCart',{
+                    productId: productId
+                }).then((response)=>{
+                    let res = response.data;
+                    if(res.status == 0){
+                        this.mdShowCart = true;
+                    }else{
+                        this.mdShow = true;
+                    }
+                })
+            },
+            closeModal(){
+                this.mdShow = false;
             }
         }
     }
